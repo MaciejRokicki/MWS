dc7376445aaf1e8e6089dcdaffd1fa4e41446584
Maciej Rokicki <rokicki.mk@gmail.com>
2021-03-20 11:30:22 +0100

    Struktura, modele

---
+++ MWS.Startup/ViewModels/PackageViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MWS.Startup.ViewModels
+{
+    public class PackageViewModel
+    {
+        public int SystemNumber { get; set; }
+        public int Specimen { get; set; }
+        public int Package { get; set; }
+        public string ArticleCode { get; set; }
+        public DimensionViewModel Dimension { get; set; }
+        public LocationViewModel Location { get; set; }
+    }
+}
---
+++ MWS.Startup/ViewModels/SectorViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MWS.Startup.ViewModels
+{
+    public class SectorViewModel
+    {
+        public int Id { get; set; }
+        public int X { get; set; } //TODO: float ???
+        public int Y { get; set; } //TODO: float ???
+        public int Z { get; set; } //TODO: float ???
+        public float Width { get; set; }
+        public float Depth { get; set; }
+        public float Height { get; set; }
+    }
+}
