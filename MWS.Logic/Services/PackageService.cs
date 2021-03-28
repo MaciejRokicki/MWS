863cec32a1cd0e9a773296f4f10aa10fea67b310
mskwarska <150536@student.uwm.edu.pl>
2021-03-28 22:20:44 +0200

    add package service

---
+++ MWS.Logic/Services/PackageService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using MWS.Dal.Interfaces;
+using MWS.Logic.Interfaces;
+using MWS.Startup.ViewModels;
+
+namespace MWS.Logic.Services
+{
+    public class PackageService : IPackageService
+    {
+        private readonly IPackageRepository packageRepository;
+
+        public PackageService(IPackageRepository packageRepository)
+        {
+            this.packageRepository = packageRepository;
+        }
+
+        public PackageDbo GetPackage(int id)
+        {
+            PackageDbo packageDbo = packageRepository.GetPackage(x => x.Id == id);
+
+            return packageDbo;
+        }
+
+    }
+}
+
