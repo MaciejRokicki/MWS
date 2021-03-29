53b2cee417c03e412afba2b9052d5a55af898e1b
MaciejRokicki <47367280+MaciejRokicki@users.noreply.github.com>
2021-03-29 11:17:28 +0200

    Revert "Feature add package service"

--- MWS.Dal/Dbos/LocationDbo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MWS.Startup.ViewModels
-{
-    public class LocationDbo
-    {
-        public int Id { get; set; }
-        public string Arrangement { get; set; }
-    }
-}
--- MWS.Startup/ViewModels/DimensionViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MWS.Startup.ViewModels
-{
-    public class DimensionViewModel
-    {
-        public float Height { get; set; }
-        public float Width { get; set; }
-        public float Depth { get; set; }
-    }
-}
